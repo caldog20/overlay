eacdc1d4fe030b76d7a6739c8c7a6fd77ac1f134
Caleb Yates <caldog20@gmail.com>
2023-12-19 12:00:35 -0600

    Start on peer

--- node/peer.go
+++ node/peer.go
@@ -1,3 +1,20 @@
 package node
 
-type Peer struct{}
+import (
+	"github.com/flynn/noise"
+	"net"
+	"net/netip"
+	"sync"
+)
+
+type Peer struct {
+	mu sync.RWMutex
+	hs *noise.HandshakeState
+	rx *noise.Cipher
+	tx *noise.Cipher
+
+	raddr *net.UDPAddr // Change later to list of endpoints and track active
+
+	node *Node // Pointer back to node for stuff
+	Ip   netip.Addr
+}
