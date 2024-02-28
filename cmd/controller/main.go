cae7ea6980ca810df6993a0fe68cd7cc257c3ae1
caleb yates <caldog20@gmail.comz>
2024-02-27 21:13:42 -0600

    fix windows CI

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/caldog20/overlay/controller/cmd"
 )
 
+// Calls root cobra command in controller/cmd/root.go
 func main() {
 	log.Println("starting controller...")
 	cmd.Execute()
